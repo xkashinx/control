2335443b2ccb9f5b78aa5930767f7a69281f9ad9
OSU-RACE <jiachen0203@gmail.com>
2018-02-25 15:33:04 -0500

    fix publisher typo

--- src/stop_node.py
+++ src/stop_node.py
@@ -5,7 +5,7 @@ from control.msg import drive_param
 from control.msg import pid_input
 
 rospy.init_node('stop_node', anonymous=True)
-pub = rospy.publisher('control/drive_parameters', drive_param, queue_size=10)
+pub = rospy.Publisher('control/drive_parameters', drive_param, queue_size=10)
 
 msg = drive_param()
 msg.velocity = 0
